ca16a897a545cc752a7edee019d3f7cf4ebd583f
Tstebbs <2201393@uad.ac.uk>
2025-12-03 14:54:26 +0000

    Added: comments

--- hephaestus/Scenes/SwordGeneration.cs
+++ hephaestus/Scenes/SwordGeneration.cs
@@ -15,6 +15,7 @@ public partial class SwordGeneration : Node3D
     bool isSwordCurved=false;
     public int numCrossSec = 10;
 
+
     public enum SwordType
     {
         STRSWORD,
@@ -27,15 +28,20 @@ public partial class SwordGeneration : Node3D
 
     public override void _Ready()
     {
-        //test parameters
+        //test parameters will be set using sliders in the future
         bladeLength = 2.0f;
         bladeHeight = 0.05f;
         bladeWidth = 0.2f;
+
+
         //genButton = GetChild<Button>(1);
         //genButton.Pressed += GeneratePressed;
 
+        //sets 2D arrays for Spline and cross section shapes
         SpinePositions = new Array<Vector2>();
         crossSecPositions = new Array<Vector2>();
+
+        //function called currently on applicattion start but will be set to button press
         generatePressed();
       
     }
@@ -47,25 +53,30 @@ public partial class SwordGeneration : Node3D
 
     private void generatePressed()
     {
+        //takes Parameters and determine sword type
         setSwordType();
+
+        //takes sword type to generate
         GenerateSwordType(currSword);
     }
 
     private void GenerateSwordType(SwordType sword)
     {
-        //createBladeSpine(bladeLength);
-
+       //unimplmented function call for curved swords
         if(isSwordCurved==true)
         {
             curvedSpine();
         }
 
+        //staight sword point generation
         if (sword == SwordType.STRSWORD)
         {
             createBladeSpine(bladeLength);
             createStraightSword2DArray(numCrossSec,bladeWidth,bladeHeight);
 
         }
+
+        //Output code to showcase cross section points for each spine point
         int count = 0;
         for (int i = 0; i < 11; i++)
         {
@@ -75,8 +86,7 @@ public partial class SwordGeneration : Node3D
                 
                 GD.Print(" cross section 4 ", j," X ", crossSecPositions[count].X, " Y ",crossSecPositions[count].Y);
                 count++;
-            }
-           
+            }  
         }
     }
 
@@ -99,11 +109,12 @@ public partial class SwordGeneration : Node3D
         }
         else
         {
-
+            currSword= SwordType.STRSWORD;
         }
 
     }
 
+    //spaces points equally along spine
     private void createBladeSpine(float length)
     {
         float spacing = length / numCrossSec;
@@ -118,13 +129,14 @@ public partial class SwordGeneration : Node3D
 
     }
 
-    private void  createStraightSword2DArray(int crossSection, float width, float height)
+    //creates diamond shape for cross section of sword for each spine point with paer towards tip
+    private void  createStraightSword2DArray(int crossSections, float width, float height)
     {
         //start 4 points of blade to be adapted to 12 later to allow tunable sharpness and fuller
         float shapeWidth = width / 2;
         float shapeHeight = width / 2;
 
-        for (int i = 0; i < crossSection+1; i++)
+        for (int i = 0; i < crossSections+1; i++)
         {
             //adds 2d cross section
            crossSecPositions.Add(new Vector2(0-shapeWidth, 0));
